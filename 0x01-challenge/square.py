b276745175462357b101bed98da58cdd0bb37593
fkadeal <infkadeal@gmail.com>
2021-09-22 00:45:46 -0700

    ?

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ codig is gagata """
 
-class square:
+class Square:
     """ class square """
       
     def __init__(self, *args, **kwargs):
@@ -26,7 +26,7 @@ class square:
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.Permiter_of_my_square())
